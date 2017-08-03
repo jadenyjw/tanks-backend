f93de749fc2ff740bccc18bd5dea2b1bd231f505
Jaden Wang <jadenyjw@gmail.com>
2017-08-03 15:13:36 -0400

    Setup project dependencies

---
+++ game/tank.js
@@ -0,0 +1,13 @@
+var TANK_WIDTH = 40;
+var TANK_HEIGHT = 40;
+var TANK_SPEED = 2;
+
+class Tank {
+
+    constructor(arg){
+        console.log(arg);
+    }
+    static getTankWidth(){
+      return TANK_WIDTH;
+    }
+};
