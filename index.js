8b5c666c2adad22354344d077f60c20bf734bb57
Jaden Wang <jadenyjw@gmail.com>
2017-08-22 11:08:07 -0400

    Incorporate shooting but a bit buggy

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class Tank {
       var bullet = new Bullet(this.angle, this.x, this.y, this.bullets.length, this.id);
       this.bullets.push(bullet);
       for(var x = 0, n = connections.length; x < n; x++){
-          connections[x].send(JSON.stringify([3, [this.id]]));
+          connections[x].send(JSON.stringify([3, [this.id, bullet.id, this.x, this.y]]));
       }
       bullet.move();
     }
@@ -110,6 +110,13 @@ class Tank {
         connections[x].send(JSON.stringify([2, [this.id, this.angle]]));
     }
   }
+
+  respawn(){
+    this.angle = Math.floor(Math.random() * 360);
+    this.x = Math.floor(Math.random() * BOARD_WIDTH);
+    this.y = Math.floor(Math.random() * BOARD_LENGTH);
+    this.canShoot = true;
+  }
 }
 
 
@@ -123,22 +130,34 @@ class Bullet {
     this.tankID = tankID;
   }
   check(){
-    for(var i = 0, n = tanks.length; i < n; i++){
-      //Check for collision with other tanks.
-      if (Math.abs(this.x - tanks[i].x) <= TANK_SIZE/2 && Math.abs(this.y - tanks[i].y) <= TANK_SIZE/2 && i != this.tankID){
-        console.log(this.tankID);
-        console.log(tanks[this.tankID]);
+    try{
+      for(var i = 0, n = tanks.length; i < n; i++){
+        //Check for collision with other tanks.
+        if (Math.abs(this.x - tanks[i].x) <= TANK_SIZE/2 && Math.abs(this.y - tanks[i].y) <= TANK_SIZE/2 && i != this.tankID){
+          console.log(this.tankID);
+          console.log(tanks[this.tankID]);
+          tanks[this.tankID].bullets.splice(this.id, 1);
+          for(var x = this.id, n = tanks[this.tankID].bullets.length; x < n; x++){
+            tanks[this.tankID].bullets[x].id = x;
+          }
+          return true;
+        }
+      }
+
+      //Check for edge of board.
+      if (this.x > BOARD_WIDTH || this.x < 0 || this.y > BOARD_LENGTH || this.y < 0){
         tanks[this.tankID].bullets.splice(this.id, 1);
+        for(var x = this.id, n = tanks[this.tankID].bullets.length; x < n; x++){
+          tanks[this.tankID].bullets[x].id = x;
+        }
         return true;
       }
+      return false;
+    }
+    catch (e){
+
     }
 
-    //Check for edge of board.
-    if (this.x > BOARD_WIDTH || this.x < 0 || this.y > BOARD_LENGTH || this.y < 0){
-      tanks[this.tankID].bullets.splice(this.id, 1);
-      return true;
-    }
-    return false;
   }
 
   move(){
@@ -147,13 +166,20 @@ class Bullet {
     this.y += Math.sin(this.angle * (Math.PI / 180)) * BULLET_SPEED;
 
     for(var x = 0, n = connections.length; x < n; x++){
-      connections[x].send(JSON.stringify([4, [this.x, this.y]]));
+      connections[x].send(JSON.stringify([4, [this.tankID, this.id, this.x, this.y]]));
     }
 
     if(!this.check()){
-      this.move();
+      var that = this
+      setTimeout(function(){that.move()}, 50)
+    }
+    else{
+      for(var x = 0, n = connections.length; x < n - 1; x++){
+        connections[x].send(JSON.stringify([7, [this.tankID, this.id]]));
+      }
     }
   }
+
 }
 
 wss.on('connection', function connection(ws, req) {
